a6307389ae1db69e8e637a41571f4fe5c160d41d
Lucas <lucaschan1020@hotmail.com>
2022-03-13 23:20:38 +0800

    added complicated friend api

--- src/routes/friend.ts
+++ src/routes/friend.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import { constants as httpConstants } from 'http2';
-import { WithId, ObjectId } from 'mongodb';
+import { ObjectId, WithId } from 'mongodb';
 import mongoClient from '../database';
-import { User, FriendshipEnum, Friend } from '../database/schema';
+import { FriendshipEnum, User } from '../database/schema';
 import Authorize, {
   AuthorizedResponse,
 } from '../middleware/authorization-middleware';
@@ -21,6 +21,23 @@ interface FriendDetail {
   avatar?: string;
   username?: string;
   discriminator?: number;
+}
+
+interface UpdateFriendRequest extends express.Request {
+  params: {
+    username: string;
+    discriminator: string;
+  };
+  body: {
+    friendship_status: FriendshipEnum;
+  };
+}
+
+interface DeleteFriendRequest extends express.Request {
+  params: {
+    username: string;
+    discriminator: string;
+  };
 }
 
 router.get(
@@ -52,22 +69,25 @@ router.get(
     } finally {
       await mongoClient.close();
     }
-    let friendList = friends.reduce((acc, curr) => {
-      acc[curr.friend_id.toString()] = {
-        friendship_status: curr.friendship_status,
+    let friendList = {} as Record<string, FriendDetail>;
+
+    friends.forEach(
+      (friend) =>
+        (friendList[friend.friend_id.toString()] = {
+          friendship_status: friend.friendship_status,
+        })
+    );
+
+    friendResult.forEach((friend) => {
+      friendList[friend._id.toString()] = {
+        ...friendList[friend._id.toString()],
+        _id: friend._id,
+        avatar: friend.avatar,
+        username: friend.username,
+        discriminator: friend.discriminator,
       };
-      return acc;
-    }, {} as Record<string, FriendDetail>);
-    friendList = friendResult.reduce((acc, curr) => {
-      acc[curr._id.toString()] = {
-        ...acc[curr._id.toString()],
-        _id: curr._id,
-        avatar: curr.avatar,
-        username: curr.username,
-        discriminator: curr.discriminator,
-      };
-      return acc;
-    }, friendList as Record<string, FriendDetail>);
+    });
+
     return res
       .status(httpConstants.HTTP_STATUS_OK)
       .json(Object.values(friendList));
@@ -81,6 +101,7 @@ router.post(
     const { username, discriminator } = req.params;
     const currentUser = res.locals.currentUser;
     let toBeFriend: WithId<User> | null = null;
+    let isRequested = false;
     if (!username || !discriminator) {
       return res.status(httpConstants.HTTP_STATUS_BAD_REQUEST).json({
         message: 'Missing username or discriminator',
@@ -90,6 +111,15 @@ router.post(
     if (isNaN(discriminator as any)) {
       return res.status(httpConstants.HTTP_STATUS_BAD_REQUEST).json({
         message: 'Discriminator must be integer',
+      });
+    }
+
+    if (
+      currentUser.username === username &&
+      currentUser.discriminator === parseInt(discriminator)
+    ) {
+      return res.status(httpConstants.HTTP_STATUS_BAD_REQUEST).json({
+        message: 'Username and discriminator must be other user',
       });
     }
 
@@ -109,39 +139,95 @@ router.post(
         });
       }
 
-      if (
-        currentUser.friends.some((friend) =>
-          friend.friend_id.equals(toBeFriend!._id)
-        )
-      ) {
-        return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
-          message: 'Already added',
-        });
+      const userFriendship = currentUser.friends.find((friend) =>
+        friend.friend_id.equals(toBeFriend!._id)
+      );
+
+      const targetFriendship = toBeFriend.friends.find((friend) =>
+        friend.friend_id.equals(currentUser._id!)
+      );
+
+      if (userFriendship || targetFriendship) {
+        if (!userFriendship) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update friend friendship status',
+          });
+        }
+
+        if (!targetFriendship) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update friend friendship status',
+          });
+        }
+
+        if (userFriendship.friendship_status !== FriendshipEnum.Requested) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update friend friendship status',
+          });
+        }
+
+        if (targetFriendship.friendship_status !== FriendshipEnum.Pending) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update friend friendship status',
+          });
+        }
+
+        const friendUpdateResult = await userCollection.updateOne(
+          {
+            _id: toBeFriend._id,
+            'friends.friend_id': currentUser._id,
+            'friends.friendship_status': FriendshipEnum.Pending,
+          },
+          { $set: { 'friends.$.friendship_status': FriendshipEnum.Friend } }
+        );
+
+        if (friendUpdateResult.modifiedCount === 0) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update friend friendship status',
+          });
+        }
+
+        const currentUserUpdateResult = await userCollection.updateOne(
+          {
+            _id: currentUser._id,
+            'friends.friend_id': toBeFriend._id,
+            'friends.friendship_status': FriendshipEnum.Requested,
+          },
+          { $set: { 'friends.$.friendship_status': FriendshipEnum.Friend } }
+        );
+
+        if (currentUserUpdateResult.modifiedCount === 0) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update your friendship status',
+          });
+        }
+
+        isRequested = true;
+      } else {
+        await userCollection.findOneAndUpdate(
+          { _id: currentUser._id },
+          {
+            $push: {
+              friends: {
+                friend_id: toBeFriend._id,
+                friendship_status: FriendshipEnum.Pending,
+              },
+            },
+          }
+        );
+
+        await userCollection.findOneAndUpdate(
+          { _id: toBeFriend._id },
+          {
+            $push: {
+              friends: {
+                friend_id: currentUser._id!,
+                friendship_status: FriendshipEnum.Requested,
+              },
+            },
+          }
+        );
       }
-
-      await userCollection.findOneAndUpdate(
-        { _id: currentUser._id },
-        {
-          $push: {
-            friends: {
-              friend_id: toBeFriend._id,
-              friendship_status: FriendshipEnum.Pending,
-            },
-          },
-        }
-      );
-
-      await userCollection.findOneAndUpdate(
-        { _id: toBeFriend._id },
-        {
-          $push: {
-            friends: {
-              friend_id: currentUser._id!,
-              friendship_status: FriendshipEnum.Requested,
-            },
-          },
-        }
-      );
     } catch (e) {
       console.log(e);
       return res.status(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
@@ -152,12 +238,343 @@ router.post(
     }
     const newFriend: FriendDetail = {
       _id: toBeFriend._id,
-      friendship_status: FriendshipEnum.Pending,
+      friendship_status: isRequested
+        ? FriendshipEnum.Friend
+        : FriendshipEnum.Pending,
       avatar: toBeFriend.avatar,
       username: toBeFriend.username,
       discriminator: toBeFriend.discriminator,
     };
     return res.status(httpConstants.HTTP_STATUS_CREATED).json(newFriend);
+  }
+);
+
+router.put(
+  '/:username/:discriminator',
+  Authorize,
+  async (req: UpdateFriendRequest, res: AuthorizedResponse) => {
+    const currentUser = res.locals.currentUser;
+    const { username, discriminator } = req.params;
+    const { friendship_status } = req.body;
+
+    if (!username || !discriminator) {
+      return res.status(httpConstants.HTTP_STATUS_BAD_REQUEST).json({
+        message: 'Missing username or discriminator',
+      });
+    }
+
+    if (isNaN(discriminator as any)) {
+      return res.status(httpConstants.HTTP_STATUS_BAD_REQUEST).json({
+        message: 'Discriminator must be integer',
+      });
+    }
+
+    if (
+      currentUser.username === username &&
+      currentUser.discriminator === parseInt(discriminator)
+    ) {
+      return res.status(httpConstants.HTTP_STATUS_BAD_REQUEST).json({
+        message: 'Username and discriminator must be other user',
+      });
+    }
+
+    if (!friendship_status && friendship_status !== FriendshipEnum.Pending) {
+      return res.status(httpConstants.HTTP_STATUS_BAD_REQUEST).json({
+        message: 'Missing friendship status',
+      });
+    }
+
+    if (
+      friendship_status !== FriendshipEnum.Friend &&
+      friendship_status !== FriendshipEnum.Blocked
+    ) {
+      return res.status(httpConstants.HTTP_STATUS_BAD_REQUEST).json({
+        message: 'Friendship status out of range',
+      });
+    }
+
+    try {
+      await mongoClient.connect();
+      const userCollection = await mongoClient
+        .db(process.env.MONGODBNAME)
+        .collection<User>('users');
+
+      const targetFriend = await userCollection.findOne({
+        username: username,
+        discriminator: parseInt(discriminator),
+      });
+
+      if (!targetFriend) {
+        return res.status(httpConstants.HTTP_STATUS_NOT_FOUND).json({
+          message: 'User not found',
+        });
+      }
+
+      if (friendship_status === FriendshipEnum.Friend) {
+        const userFriendship = currentUser.friends.find((friend) =>
+          friend.friend_id.equals(targetFriend._id)
+        );
+
+        if (!userFriendship) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Friendship is not established',
+          });
+        }
+
+        const targetFriendship = targetFriend.friends.find((friend) =>
+          friend.friend_id.equals(currentUser._id!)
+        );
+
+        if (!targetFriendship) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Friendship is not established',
+          });
+        }
+
+        if (
+          userFriendship.friendship_status !== FriendshipEnum.Requested ||
+          targetFriendship.friendship_status !== FriendshipEnum.Pending
+        ) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update friend friendship status',
+          });
+        }
+
+        const friendUpdateResult = await userCollection.updateOne(
+          {
+            _id: targetFriend._id,
+            'friends.friend_id': currentUser._id,
+            'friends.friendship_status': FriendshipEnum.Pending,
+          },
+          { $set: { 'friends.$.friendship_status': FriendshipEnum.Friend } }
+        );
+
+        if (friendUpdateResult.modifiedCount === 0) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update friend friendship status',
+          });
+        }
+
+        const currentUserUpdateResult = await userCollection.updateOne(
+          {
+            _id: currentUser._id,
+            'friends.friend_id': targetFriend._id,
+            'friends.friendship_status': FriendshipEnum.Requested,
+          },
+          { $set: { 'friends.$.friendship_status': FriendshipEnum.Friend } }
+        );
+
+        if (currentUserUpdateResult.modifiedCount === 0) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update your friendship status',
+          });
+        }
+      }
+
+      if (friendship_status === FriendshipEnum.Blocked) {
+        const userFriendship = currentUser.friends.find((friend) =>
+          friend.friend_id.equals(targetFriend._id)
+        );
+
+        if (userFriendship?.friendship_status === FriendshipEnum.Blocked) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update friend friendship status',
+          });
+        }
+
+        const currentUserUpdateResult = await userCollection.updateOne(
+          {
+            _id: currentUser._id,
+            'friends.friend_id': targetFriend._id,
+          },
+          { $set: { 'friends.$.friendship_status': FriendshipEnum.Blocked } }
+        );
+
+        if (currentUserUpdateResult.modifiedCount === 0) {
+          await userCollection.findOneAndUpdate(
+            { _id: currentUser._id },
+            {
+              $push: {
+                friends: {
+                  friend_id: targetFriend._id!,
+                  friendship_status: FriendshipEnum.Blocked,
+                },
+              },
+            }
+          );
+        }
+
+        const targetFriendship = targetFriend.friends.find((friend) =>
+          friend.friend_id.equals(currentUser._id!)
+        );
+
+        if (targetFriendship?.friendship_status !== FriendshipEnum.Blocked) {
+          const friendUpdateResult = await userCollection.updateOne(
+            {
+              _id: targetFriend._id,
+            },
+            { $pull: { friends: { friend_id: currentUser._id } } }
+          );
+
+          if (friendUpdateResult.modifiedCount === 0) {
+            return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+              message: 'Failed to update your friendship status',
+            });
+          }
+        }
+      }
+    } catch (e) {
+      console.log(e);
+      return res.status(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
+        message: 'Something went wrong',
+      });
+    } finally {
+      await mongoClient.close();
+    }
+    return res.status(httpConstants.HTTP_STATUS_NO_CONTENT).json();
+  }
+);
+
+router.delete(
+  '/:username/:discriminator',
+  Authorize,
+  async (req: DeleteFriendRequest, res: AuthorizedResponse) => {
+    const currentUser = res.locals.currentUser;
+    const { username, discriminator } = req.params;
+
+    if (!username || !discriminator) {
+      return res.status(httpConstants.HTTP_STATUS_BAD_REQUEST).json({
+        message: 'Missing username or discriminator',
+      });
+    }
+
+    if (isNaN(discriminator as any)) {
+      return res.status(httpConstants.HTTP_STATUS_BAD_REQUEST).json({
+        message: 'Discriminator must be integer',
+      });
+    }
+
+    if (
+      currentUser.username === username &&
+      currentUser.discriminator === parseInt(discriminator)
+    ) {
+      return res.status(httpConstants.HTTP_STATUS_BAD_REQUEST).json({
+        message: 'Username and discriminator must be other user',
+      });
+    }
+
+    try {
+      await mongoClient.connect();
+      const userCollection = await mongoClient
+        .db(process.env.MONGODBNAME)
+        .collection<User>('users');
+
+      const targetFriend = await userCollection.findOne({
+        username: username,
+        discriminator: parseInt(discriminator),
+      });
+
+      if (!targetFriend) {
+        return res.status(httpConstants.HTTP_STATUS_NOT_FOUND).json({
+          message: 'User not found',
+        });
+      }
+
+      const userFriendship = currentUser.friends.find((friend) =>
+        friend.friend_id.equals(targetFriend._id)
+      );
+
+      if (!userFriendship) {
+        return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+          message: 'Friendship is not established',
+        });
+      }
+
+      const targetFriendship = targetFriend.friends.find((friend) =>
+        friend.friend_id.equals(currentUser._id!)
+      );
+
+      if (
+        userFriendship.friendship_status === FriendshipEnum.Pending &&
+        targetFriendship?.friendship_status === FriendshipEnum.Requested
+      ) {
+        const friendUpdateResult = await userCollection.updateOne(
+          {
+            _id: targetFriend._id,
+          },
+          { $pull: { friends: { friend_id: currentUser._id } } }
+        );
+
+        if (friendUpdateResult.modifiedCount === 0) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update your friendship status',
+          });
+        }
+      }
+
+      if (userFriendship.friendship_status === FriendshipEnum.Requested) {
+        if (!targetFriendship) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Friendship is not established',
+          });
+        }
+
+        if (targetFriendship.friendship_status !== FriendshipEnum.Pending) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update friend friendship status',
+          });
+        }
+      }
+
+      if (userFriendship.friendship_status === FriendshipEnum.Friend) {
+        if (!targetFriendship) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Friendship is not established',
+          });
+        }
+
+        if (targetFriendship.friendship_status !== FriendshipEnum.Friend) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update friend friendship status',
+          });
+        }
+
+        const friendUpdateResult = await userCollection.updateOne(
+          {
+            _id: targetFriend._id,
+          },
+          { $pull: { friends: { friend_id: currentUser._id } } }
+        );
+
+        if (friendUpdateResult.modifiedCount === 0) {
+          return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+            message: 'Failed to update your friendship status',
+          });
+        }
+      }
+
+      const currentUserUpdateResult = await userCollection.updateOne(
+        {
+          _id: currentUser._id,
+        },
+        { $pull: { friends: { friend_id: targetFriend._id } } }
+      );
+
+      if (currentUserUpdateResult.modifiedCount === 0) {
+        return res.status(httpConstants.HTTP_STATUS_CONFLICT).json({
+          message: 'Failed to update your friendship status',
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      return res.status(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
+        message: 'Something went wrong',
+      });
+    } finally {
+      await mongoClient.close();
+    }
+
+    return res.status(httpConstants.HTTP_STATUS_NO_CONTENT).json();
   }
 );
 
