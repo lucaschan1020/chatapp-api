78137985c59f28082aaec89a2a2473650313df15
lucaschan1020 <lucaschan1020@hotmail.com>
2023-02-27 14:53:19 +0800

    removed env for collections naming

--- src/database/index.ts
+++ src/database/index.ts
@@ -11,24 +11,14 @@ export const collections: {
 export async function connectToDatabase() {
   const mongoDBUri = process.env.MONGODB_URI;
   const mongoDBName = process.env.MONGODB_NAME;
-  const usersCollectionName = process.env.USERS_COLLECTION_NAME;
-  const privateChannelsCollectionName =
-    process.env.PRIVATECHANNELS_COLLECTION_NAME;
-  const chatBucketsCollectionName = process.env.CHATBUCKETS_COLLECTION_NAME;
+  const usersCollectionName = 'users';
+  const privateChannelsCollectionName = 'privateChannels';
+  const chatBucketsCollectionName = 'chatBuckets';
   if (!mongoDBUri) {
     throw new Error('MONGODB_URI not found in env');
   }
   if (!mongoDBName) {
     throw new Error('MONGODB_NAME not found in env');
-  }
-  if (!usersCollectionName) {
-    throw new Error('USERS_COLLECTION_NAME not found in env');
-  }
-  if (!privateChannelsCollectionName) {
-    throw new Error('PRIVATECHANNELS_COLLECTION_NAME not found in env');
-  }
-  if (!chatBucketsCollectionName) {
-    throw new Error('CHATBUCKETS_COLLECTION_NAME not found in env');
   }
 
   const client = new mongoDB.MongoClient(mongoDBUri);
